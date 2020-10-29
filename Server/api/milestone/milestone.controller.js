a417ec38450c722a48c304ea76ca2242124813c2
do02reen24 <do02reen24@gmail.com>
2020-10-29 14:41:59 +0900

    fix : 존재하지 않는 마일스톤에 대한 조회 요청 예외처리 추가
    - 존재하지 않는 마일스톤 요청에 대해서도 성공(200)을 반환하던 부분에 대해 수정을 진행
    - 존재하지 않는 경우 실패(400) 상태와 메시지를 반환하도록 변경함

--- Server/api/milestone/milestone.controller.js
+++ Server/api/milestone/milestone.controller.js
@@ -24,9 +24,14 @@ module.exports = {
   getMilestone: (req, res) => {
     getMilestone(req, (err, results) => {
       const failMessage = '요청한 마일스톤 조회에 실패했습니다.';
+      const failMessageById = '존재하지 않는 마일스톤에 대한 조회 요청입니다.';
 
       if (err) {
         return res.status(400).json(failResponse(failMessage));
+      }
+
+      if (results.length === 0) {
+        return res.status(400).json(failResponse(failMessageById));
       }
 
       return res.status(200).json(successResponse(results));
