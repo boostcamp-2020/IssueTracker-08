bbbad30c0f01ecb23d539a322a918e01e7cae37d
kimn <rile1036@naver.com>
2020-10-29 18:31:59 +0900

    Feat : #9 이슈, milestone 레이아웃 변경 - 이슈 설명 Line Break 설정 - milestone border 추가 및 길이에 따른 넓이 설정

--- IssueTracker/IssueTracker/Scenes/IssueList/Views/IssueListCollectionViewCell.swift
+++ IssueTracker/IssueTracker/Scenes/IssueList/Views/IssueListCollectionViewCell.swift
@@ -8,10 +8,16 @@
 import UIKit
 
 class IssueListCollectionViewCell: UICollectionViewCell {
-    
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var milestoneLabel: UILabel!
     @IBOutlet var labelCollection: [UILabel]!
     
+    func configure() {
+        milestoneLabel.layer.cornerRadius = 5
+        milestoneLabel.layer.borderWidth = 1
+        milestoneLabel.layer.borderColor = UIColor.lightGray.cgColor
+        
+        milestoneLabel.widthAnchor.constraint(equalToConstant: milestoneLabel.frame.width + 10).isActive = true
+    }
 }
