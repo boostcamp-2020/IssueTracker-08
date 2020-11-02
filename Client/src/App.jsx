fa6bed27790ecccf21cab332dc316ef96f9eb1bb
do02reen24 <do02reen24@gmail.com>
2020-11-02 16:12:36 +0900

    feat : 프론트 프로젝트 생성 - 프론트 프로젝트를 생성함 - 초기 프론트의 css는 기본 css를 사용

---
+++ Client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+    </div>
+  );
+};
+
+export default App;
