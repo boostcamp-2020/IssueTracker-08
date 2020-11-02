20cbfc62d04fbc90c6c6494d925c7c0b775ba05a
yancy <yeeji199533@gmail.com>
2020-11-02 17:04:22 +0900

    Feat : #139 swagger-ui-express swagger-jsdoc 모듈 추가
    swagger 문서 개발을 위한 모듈을 추가했습니다.

--- Server/app.js
+++ Server/app.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 const indexRouter = require('./routes/index');
+
+const swaggerDefinition = {
+  info: {
+    title: 'IssueTracker-08',
+    version: '1.0.0',
+    description: 'IssueTracker API Document',
+  },
+  basePath: '/api',
+};
+
+const options = {
+  swaggerDefinition,
+  apis: [
+    './api/issue/issue.router.js',
+    './api/milestone/milestone.router.js',
+    './api/signin/signin.router.js',
+    './api/user/user.router.js',
+  ],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
 
 const app = express();
 
@@ -12,7 +35,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/', indexRouter);
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 // error handler
 app.use(function (err, req, res, next) {
   res.locals.message = err.message;
