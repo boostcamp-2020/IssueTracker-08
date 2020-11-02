a538fde7806a534b4fb82bbe794690bb93722be2
Sue Cho <suejeong97@naver.com>
2020-11-02 16:04:59 +0900

    Fix : #9 Update ViewModel
    - Format description
    - Limit max returned labels to 2

--- IssueTracker/IssueTracker/Scenes/IssueList/IssueListPresenter.swift
+++ IssueTracker/IssueTracker/Scenes/IssueList/IssueListPresenter.swift
@@ -23,17 +23,42 @@ extension IssueListPresenter: IssueListPresentationLogic {
     func presentFetchedIssues(response: ListIssues.FetchLists.Response) {
         var displayedIssues: [ListIssues.FetchLists.ViewModel.DisplayedIssue] = []
         for issue in response.issues {
-            var description = issue.content
-            if description.isEmpty { description = "No description provided" }
+            let description = configureDescription(text: issue.content)
+            let labels = configureLabel(texts: issue.label)
             let displayedIssue = ListIssues.FetchLists.ViewModel.DisplayedIssue(
                 title: issue.title,
                 content: description,
                 milestone: issue.milestone,
-                label: issue.label
+                label: labels
             )
+            
+            
+//            var description = issue.content
+//            if description.isEmpty { description = "No description provided" }
+//            let displayedIssue = ListIssues.FetchLists.ViewModel.DisplayedIssue(
+//                title: issue.title,
+//                content: description,
+//                milestone: issue.milestone,
+//                label: issue.label
+//            )
             displayedIssues.append(displayedIssue)
         }
         let viewModel = ListIssues.FetchLists.ViewModel(displayedIssues: displayedIssues)
         viewController?.displayFetchedOrders(viewModel: viewModel)
+    }
+    
+    private func configureDescription(text: String?) -> String {
+        if let description = text {
+            return description
+        }
+        return "No description provided"
+    }
+    
+    private func configureLabel(texts: [String]?) -> [String]? {
+        if let labels = texts {
+            if labels.count > 2 { return [labels[0], labels[1]] }
+            else { return labels }
+        }
+        return nil
     }
 }
