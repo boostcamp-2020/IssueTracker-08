cc0e6a39bcf397f4ec18507a0ac4be9477f67f94
do02reen24 <do02reen24@gmail.com>
2020-11-05 01:08:16 +0900

    Refactor : 불필요한 scss 파일 제거 - 이전에 작성했던 scss 파일을 제거함

--- Client/src/pages/login/LoginPage.jsx
+++ Client/src/pages/login/LoginPage.jsx
@@ -1,39 +1,9 @@
-import React, { useState, Component } from 'react';
-
-import './LoginPage.scss';
-import * as config from '../../config';
+import React from 'react';
 
 export default function LoginPage() {
   return (
-      <div class="container">
-        <div class="title">
-          <h1>이슈 트래커</h1>
-        </div>
-
-        <div class="login">
-          <div class="id">
-            <p>아이디</p>
-            <input type="text"></input>
-          </div>
-
-          <div class="password">
-            <p>비밀번호</p>
-            <input type="password"></input>
-          </div>
-
-          <div class="routes">
-            <a href="">로그인</a>
-            <a href="">회원가입</a>
-          </div>
-
-          <a href={config.BASE_URL + "auth/github/"}>
-            <div class="github-login">
-              <p>Sign in with GitHub</p>
-              <img src="/images/github.svg"></img>
-            </div>
-          </a>
-        </div>
-
-      </div>
+    <div class="container">
+      <p>로그인 페이지 입니다.</p>
+    </div>
   );
 }
--- Client/src/shared/App.jsx
+++ Client/src/shared/App.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Home, About } from '../pages';
-import Menu from '../components/Menu'
+import Menu from '../components/Menu';
+import LoginPage from '../pages/login/LoginPage';
 
 const App = () => {
-  return (
-    <div>
-      <Menu></Menu>
-      <Route exact path="/" component={Home}/>
-      <Switch>
-        <Route path="/about/:name" component={About}/>
-        <Route path="/about" component={About}/>
-      </Switch>
-    </div>
-  );
+  const isLoggedIn = true; // TODO : setLoginState
+  if (isLoggedIn) {
+    return (
+      <div>
+        <Menu></Menu>
+        <Route exact path="/" component={Home} />
+        <Switch>
+          <Route path="/about/:name" component={About} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </div>
+    );
+  }
+  return <LoginPage />;
 };
 
 export default App;
