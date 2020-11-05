26ce2f10a7fadc72632629ae330a66d9f39e9612
Sue Cho <suejeong97@naver.com>
2020-11-06 04:46:25 +0900

    Fix : #155 Swipe로 close 후 title 에러 수정
    - 전체 화면에서 swipe를 통해 close 하면 -1 Selected 라고 타이틀 바뀌는 것 되는 것 고침

--- IssueTracker/IssueTracker/Scenes/IssueList/IssueListViewController.swift
+++ IssueTracker/IssueTracker/Scenes/IssueList/IssueListViewController.swift
@@ -18,8 +18,8 @@ final class IssueListViewController: UIViewController {
     // MARK:- IBOutlets
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var newIssueButton: CustomAddButton!
-    @IBOutlet weak var issueListCollectionView: UICollectionView!
     @IBOutlet weak var closeIssueButton: UIButton!
+    @IBOutlet weak var issueListCollectionView: UICollectionView!
    
     // MARK:- Properties
     var filterData = ListFilter.IssueFilterData()
@@ -34,13 +34,15 @@ final class IssueListViewController: UIViewController {
     }
     var selectedItems: Int = 0 {
         willSet {
-            titleLabel.text = "\(newValue) Selected"
-            if newValue > 0 {
-                closeIssueButton.isEnabled = true
-                navigationItem.leftBarButtonItem?.title = "Deselect All"
-            } else {
-                closeIssueButton.isEnabled = false
-                navigationItem.leftBarButtonItem?.title = "Select All"
+            if isEditing {
+                titleLabel.text = "\(newValue) Selected"
+                if newValue > 0 {
+                    closeIssueButton.isEnabled = true
+                    navigationItem.leftBarButtonItem?.title = "Deselect All"
+                } else {
+                    closeIssueButton.isEnabled = false
+                    navigationItem.leftBarButtonItem?.title = "Select All"
+                }
             }
         }
     }
@@ -254,7 +256,6 @@ extension IssueListViewController: UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        //print(displayedIssues[indexPath.row].issueId) // tag
         if isEditing {
             selectedItems += 1
         } else {
