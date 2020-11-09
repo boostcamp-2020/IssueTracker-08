2a10c9924e416d7eb7326016c67a23f31f989de4
Yejikwon <yeeji77@naver.com>
2020-11-09 20:12:33 +0900

    Feat : #115 issue list 목록 컴포넌트 & 디자인 구현
    리스트의 목록을 표현하는 컴포넌트를 구현했습니다.

---
+++ Client/src/components/issue/issueList.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+
+import { GET_OPEN_ISSUE } from '../../utils/api';
+import { getOptions } from '../../utils/fetchOptions';
+
+import Style from './style/ListStyle';
+import IssueTemplate from './issueListTemplate';
+
+const IssueList = () => {
+  const [issues, setIssues] = useState([]);
+
+  const loadOpenIssues = async () => {
+    const response = await fetch(GET_OPEN_ISSUE, getOptions);
+    const result = await response.json();
+    setIssues(result.data);
+    return result.data;
+  };
+
+  useEffect(() => {
+    loadOpenIssues();
+  }, []);
+
+  return (
+    <div className="list-wrapper">
+      {issues.length > 0 ? (
+        issues.map((issue) => (
+          <IssueTemplate key={issue.issueId} issue={issue} />
+        ))
+      ) : (
+        <Style.NoResultsBox>
+          <span>No results matched your search.</span>
+        </Style.NoResultsBox>
+      )}
+    </div>
+  );
+};
+
+export default IssueList;
---
+++ Client/src/components/issue/style/ListStyle.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export default {
+  NoResultsBox: styled.div`
+    height: 300px;
+    display: flex;
+    font-size: 25px;
+    font-weight: bold;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid #ebecef;
+    border-radius: 0 0 6px 6px;
+    border-top: none;
+    padding-bottom: 24px;
+  `,
+};
