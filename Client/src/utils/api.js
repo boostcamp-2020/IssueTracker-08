0ac5da2d5a3876391a5f3d854e09b5bb231e168c
Yejikwon <yeeji77@naver.com>
2020-11-09 17:01:35 +0900

    Feat : #115 default로 open된 issue를 가져오기 위한 API uri 추가
    open된 issue를 가져오기 위해 API uri를 파일에 추가했습니다.

--- Client/src/utils/api.js
+++ Client/src/utils/api.js
@@ -1,3 +1,5 @@
 import * as config from '../config';
 
 export const POST_ISSUE = config.BASE_URL + 'api/issues';
+export const GET_OPEN_ISSUE = config.BASE_URL + 'api/issues/open';
+export const GET_CLOSED_ISSUE = config.BASE_URL + 'api/issues/closed';
