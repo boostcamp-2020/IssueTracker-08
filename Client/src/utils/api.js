e37da1d58cb2c9b2fef7305e738d503867cd4ce1
Yejikwon <yeeji77@naver.com>
2020-11-10 23:25:47 +0900

    Feat : 전체 user를 가져오는 api 정보 추가

--- Client/src/utils/api.js
+++ Client/src/utils/api.js
@@ -3,3 +3,5 @@ import * as config from '../config';
 export const POST_ISSUE = config.BASE_URL + 'api/issues';
 export const GET_OPEN_ISSUE = config.BASE_URL + 'api/issues/open';
 export const GET_CLOSED_ISSUE = config.BASE_URL + 'api/issues/closed';
+
+export const GET_ALL_USERS = config.BASE_URL + 'api/users';
