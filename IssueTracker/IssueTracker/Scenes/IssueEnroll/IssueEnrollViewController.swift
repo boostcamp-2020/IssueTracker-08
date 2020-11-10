497ba4c08144d3d1c19df0b7a58c884089ad2efb
Sue Cho <suejeong97@naver.com>
2020-11-11 01:54:43 +0900

    Feat : #55 Insert Photos Library
    - Menu item의 insert photo 선택 시 photos library가 열리며 image url을 추출

--- IssueTracker/IssueTracker/Scenes/IssueEnroll/IssueEnrollViewController.swift
+++ IssueTracker/IssueTracker/Scenes/IssueEnroll/IssueEnrollViewController.swift
@@ -21,6 +21,8 @@ class IssueEnrollViewController: UIViewController {
     // MARK:- Properties
     private var markdown = ["Markdown", "Preview"]
     private let markdownView = MarkdownView()
+    private let menuController = UIMenuController.shared
+    private let imagePicker = UIImagePickerController()
     
     // MARK:- View Lifecycle
     override func viewDidLoad() {
@@ -33,10 +35,12 @@ class IssueEnrollViewController: UIViewController {
         view.endEditing(true)
     }
     
+    // MARK:- IBActions
     @IBAction func saveButton(_ sender: Any) {
         // TO DO:
             // Send to Server
     }
+    
 }
 
 // MARK:- Setup
@@ -46,29 +50,43 @@ extension IssueEnrollViewController {
         titleView.layer.addBorder([.top, .bottom], color: UIColor.systemGray5, width: 1)
         textDescription.layer.borderWidth = 1
         textDescription.layer.borderColor = UIColor.systemGray.cgColor
-        markdownConstraint()
-        placeholderSetting()
-        configureNotification()
+        setupPlaceholder()
+        setupMenuController()
+        setupImagePicker()
+        addMarkdownConstraints()
+        addObservers()
     }
     
-    private func configureNotification() {
+    private func addObservers() {
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
-    private func markdownConstraint() {
+    private func addMarkdownConstraints() {
         markdownUIView.addSubview(markdownView)
         markdownView.translatesAutoresizingMaskIntoConstraints = false
         markdownView.topAnchor.constraint(equalTo: markdownUIView.topAnchor).isActive = true
         markdownView.leadingAnchor.constraint(equalTo: markdownUIView.leadingAnchor).isActive = true
         markdownView.trailingAnchor.constraint(equalTo: markdownUIView.trailingAnchor).isActive = true
         markdownView.bottomAnchor.constraint(equalTo: markdownUIView.bottomAnchor).isActive = true
+        
         markdownSegment.addTarget(self, action: #selector(updateView), for: .valueChanged)
+    }
+    
+    private func setupMenuController() {
+        menuController.showMenu(from: view, rect: CGRect.zero)
+        menuController.arrowDirection = UIMenuController.ArrowDirection.default
+        let menuItem1: UIMenuItem = UIMenuItem(title: "Insert Photo", action: #selector(menuInsertPhoto(sender:)))
+        menuController.menuItems = [menuItem1]
+    }
+    
+    private func setupImagePicker() {
+        imagePicker.delegate = self
     }
     
 }
 
-// MARK:- Selector
+// MARK:- Selector @objc
 extension IssueEnrollViewController {
     
     @objc private func keyboardWillShow(_ notification: Notification) {
@@ -83,7 +101,7 @@ extension IssueEnrollViewController {
         textViewBottomConstraint.constant = (keyboardHeight - tabbarHeight) - 145 + 14
     }
     
-    @objc func keyboardWillHide(notification: NSNotification) {
+    @objc private func keyboardWillHide(notification: NSNotification) {
         self.view.frame.origin.y = 0
         textViewBottomConstraint.constant = 14
     }
@@ -99,31 +117,33 @@ extension IssueEnrollViewController {
             if markdownView.subviews.count > 0 {
                 markdownView.subviews[0].removeFromSuperview()
             }
-            
             if textDescription.textColor != UIColor.lightGray {
                 markdownView.onTouchLink = { [weak self] request in
                   guard let url = request.url else { return false }
-
-                  if url.scheme == "file" {
-                    return true
-                  } else if url.scheme == "https" {
+                  if url.scheme == "file" { return true }
+                  else if url.scheme == "https" {
                     let safari = SFSafariViewController(url: url)
                     self?.present(safari, animated: true, completion: nil)
                     return false
-                  } else {
-                    return false
                   }
+                  else { return false }
                 }
-                
                 markdownView.load(markdown: textDescription.text, enableImage: true)
             }
         }
     }
     
+    @objc private func menuInsertPhoto(sender: UIMenuItem) {
+        print("Insert Photo Pressed")
+        openPhotosLibrary()
+    }
+    
 }
 
+// MARK:- UITextViewDelegate
 extension IssueEnrollViewController: UITextViewDelegate {
-    private func placeholderSetting() {
+    
+    private func setupPlaceholder() {
         textDescription.delegate = self
         textDescription.text = "코멘트는 여기에 작성하세요."
         textDescription.textColor = UIColor.lightGray
@@ -142,4 +162,33 @@ extension IssueEnrollViewController: UITextViewDelegate {
             textDescription.textColor = UIColor.lightGray
         }
     }
+    
+//    override func target(forAction action: Selector, withSender sender: Any?) -> Any? {
+//        let cut = #selector(cut(_:))
+//        let copy = #selector(copy(_:))
+//        let watnedActions = [cut, copy, #selector(menuInsertPhoto(sender:))]
+//
+//        if watnedActions.contains(action) { return true }
+//        else { return false }
+//    }
+    
+}
+
+extension IssueEnrollViewController: UIImagePickerControllerDelegate,
+                                     UINavigationControllerDelegate {
+    func openPhotosLibrary() {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // TODO:
+            // Send to server and receive a link
+            // 3가지 info key가 들어가 있는데 어떤 정보를 뽑아서 보낼지는 더 알아봐야 함
+        if let imageURL = info[UIImagePickerController.InfoKey.imageURL] as? URL {
+            textDescription.text.append("\n\(imageURL)")
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
