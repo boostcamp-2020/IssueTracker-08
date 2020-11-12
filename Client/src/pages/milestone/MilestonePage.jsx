a2cdc98a76391cc1dbf01dd2207b0ab1271cb9f4
do02reen24 <do02reen24@gmail.com>
2020-11-12 14:28:00 +0900

    Feat : #226 마일스톤 상단바 UI 디자인 - 실제 데이터를 받아오진 않고 UI 요소를 뿌려주기만 하는 코드임

--- Client/src/pages/milestone/MilestonePage.jsx
+++ Client/src/pages/milestone/MilestonePage.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { MilestoneIcon, CheckIcon } from '@primer/octicons-react';
+
 import Container from '../../components/shared/container/Container';
 import ItemContainer from '../../components/shared/container/ItemContainer';
 import ItemHeader from '../../components/shared/container/ItemHeader';
+import MilestoneList from '../../components/milestone/MilestoneList';
 import Menu from '../../components/shared/container/Menu';
 import NewButton from '../../components/shared/button/NewButton';
+import styled from 'styled-components';
+
+const MilestoneMenuContainer = styled.div`
+  display: flex;
+  margin: 0px 6px;
+  align-items: center;
+`;
+
+const Title = styled.p`
+  margin-left: 6px;
+`;
 
 export default function MilestonePage() {
   return (
@@ -12,7 +26,17 @@ export default function MilestonePage() {
         <NewButton link="/milestone/post" name="milestone" />
       </Menu>
       <ItemContainer>
-        <ItemHeader />
+        <ItemHeader>
+          <MilestoneMenuContainer>
+            <MilestoneIcon />
+            <Title>{0 + ' Open'}</Title>
+          </MilestoneMenuContainer>
+          <MilestoneMenuContainer>
+            <CheckIcon />
+            <Title>{0 + ' Closed'}</Title>
+          </MilestoneMenuContainer>
+        </ItemHeader>
+        <MilestoneList />
       </ItemContainer>
     </Container>
   );
